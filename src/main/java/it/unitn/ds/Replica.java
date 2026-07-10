5ae5a80f7fdeac0d6b0441a507332f024b657681
KeithLeoni <keith.leoni03@gmail.com>
2026-07-10 12:32:05 +0200

    Started timeouts handling

--- src/main/java/it/unitn/ds/Replica.java
+++ src/main/java/it/unitn/ds/Replica.java
@@ -1,19 +1,16 @@
 package it.unitn.ds;
 
-import akka.actor.ActorRef;
-import akka.actor.Props;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 import akka.actor.ActorRef;
 import akka.actor.Cancellable;
-import akka.actor.Props; // from doc: something that can be cancelled, with method .cancel()
-import scala.concurrent.duration.Duration;
-import java.util.concurrent.TimeUnit;
+import akka.actor.Props;
+import scala.concurrent.duration.Duration; // from doc: something that can be cancelled, with method .cancel()
 
 public class Replica extends AbstractReplica {
     private Map<Integer, ActorRef> group;
@@ -35,13 +32,14 @@ public class Replica extends AbstractReplica {
 
     private Map<Messages.NodeClock, Messages.UpdateData> coordinatorProposals = new HashMap<>();
 
-    private Cancellable heartbeatTimeout;
+    // private Cancellable heartbeatTimeout;
 
     // init timers to detect coordinator crashes
     private Cancellable heartbeatTimer = null; // wait for heartbeat message from coordinator, re-init once received
     private Cancellable updateTimer = null; // starts after forwarding write request to the coordinator, wait for UPDATE message from coordinator
     private Cancellable writeOkTimer = null; // starts after sending ACK in response to UPDATE message, wait for WRITEOK message from coordinator
 
+    private int timer_duration = 5;
 
     public Replica(int id) {
         this(id, AbstractReplica.MIN_LATENCY, AbstractReplica.MAX_LATENCY, AbstractReplica.COORDINATOR_BEAT_INTERVAL,
@@ -77,6 +75,9 @@ public class Replica extends AbstractReplica {
                 () -> new Replica(id, minLatency, maxLatency, coordinatorBeatInterval, Optional.ofNullable(listener)));
     }
 
+    // handling of the UpdateRequest message
+    // - coordinator actions -> handle request + forward Update message
+    // - replica actions -> forward message to coordinator + handle Update timer 
     private final void handleUpdateRequest(Messages.UpdateRequest _msg) throws Exception {
         if (this.id == coordinatorId) {
             // debug("Replica " + this.id + " is the coordinator and received UpdateRequest
@@ -84,7 +85,6 @@ public class Replica extends AbstractReplica {
             // THIS IS THE COORDINATOR
             // - forward to other replicas UPDATE MESSAGE
             // - save client in myClients and updateClients with current clock
-            //
 
             this.seqNum++;
             Messages.NodeClock updateClock = new Messages.NodeClock(this.epoch, this.seqNum);
@@ -118,36 +118,61 @@ public class Replica extends AbstractReplica {
             // - add client to the list of clients without clock
             // debug("Replica " + this.id + " forwarding UpdateRequest to coordinator " +
             // coordinatorId);
+
+            // add client ot the list of node clients
+            // myClients -> <ActorRef, Messages.NodeClock> -> NodeClock is null, will be assigned by coordinator
+            this.myClients.put(_msg.client, null);
+
             Messages.UpdateRequest forwardMsg = new Messages.UpdateRequest(_msg.index, _msg.value,
                     _msg.client, true);
             group.get(coordinatorId).tell(forwardMsg, getSelf());
 
-            myClients.put(_msg.client, null);
+            // when the node sends UpdateRequest to the coordinator it starts waiting for the Update message, so the updateTimer is started
+            updateTimer = getContext().system().scheduler().scheduleOnce(
+                Duration.create(timer_duration, TimeUnit.SECONDS), // timer duration
+                getSelf(),                                         // destination (self)
+                new Messages.UpdateTimeout(),                      // message that will be received, here UpdateTimeout
+                getContext().dispatcher(),                         // dispatcher
+                getSelf()                                          // sender (self)
+            );
         }
-        // TODO: handle timeout (I guess)
     }
 
+    // handle Update message (nodes)
     private final void handleUpdate(Messages.Update _msg) throws Exception {
         // debug("Replica " + this.id + " received Update from coordinator " +
         // coordinatorId + " with clock "
         // + _msg.clock);
+
+        // received Update message, cancel the Update timer!
+        updateTimer.cancel();
+
         this.toCommitQueue.put(_msg.clock, new Messages.UpdateData(_msg.index, _msg.value));
 
         updateClients.put(_msg.clock, _msg.client);
 
+        // if client is this node client and the message NodeClock associated is still null, must be initialized now that it has the clock value assigned by the coordionator
         if (myClients.containsKey(_msg.client) && myClients.get(_msg.client) == null) {
             myClients.put(_msg.client, _msg.clock);
         }
 
-        // send ACK back to the coordinator
+        // send ACK back to the coordinator 
         group.get(coordinatorId).tell(new Messages.Ack(_msg.clock), getSelf());
 
-        // TODO: handle timeout (I guess)
+        // when the node sends ACK to the coordinator it starts waiting for the WriteOk message, so the writeOkTimer is started
+        writeOkTimer = getContext().system().scheduler().scheduleOnce(
+            Duration.create(timer_duration, TimeUnit.SECONDS), // timer duration
+            getSelf(),                                         // destination (self)
+            new Messages.WriteOkTimeout(),                     // message that will be received, here WriteOkTimeout
+            getContext().dispatcher(),                         // dispatcher
+            getSelf()                                          // sender (self)
+        );
     }
 
     private final void handleAck(Messages.Ack _msg) throws Exception {
         // debug("Replica " + this.id + " received Ack from replica " + getSender() + "
         // for clock " + _msg.clock);
+
         // incerment number of received ack for the _msg.NodeClock
         int currentCount = this.ackCounters.getOrDefault(_msg.clock, 0);
         currentCount++;
@@ -203,6 +228,11 @@ public class Replica extends AbstractReplica {
     private final void handleWriteOk(Messages.WriteOk _msg) throws Exception {
         // debug("Replica " + this.id + " received WriteOk from replica " + getSender()
         // + " for clock " + _msg.clock);
+
+        // received WriteOk message, cancel the WriteOk timer!
+        writeOkTimer.cancel();
+
+
         // update internal state with the new values
         Messages.UpdateData toCommitData = toCommitQueue.remove(_msg.clock);
         if (toCommitData != null) {
@@ -232,29 +262,15 @@ public class Replica extends AbstractReplica {
         tell(new AbstractClient.ReadResult(true, _msg.index, value, this.id), _msg.client);
     }
 
-    private final void handleHeartbeat(Messages.Heartbeat _msg) {
-        for (Map.Entry<Integer, ActorRef> entry : group.entrySet()) {
-            if (entry.getKey() != this.id) {
-                entry.getValue().tell(new Messages.Heartbeat(), getSelf());
-            }
-        }
-    }
+    // private final void handleHeartbeat(Messages.Heartbeat _msg) {
+    //     for (Map.Entry<Integer, ActorRef> entry : group.entrySet()) {
+    //         if (entry.getKey() != this.id) {
+    //             entry.getValue().tell(new Messages.HeartbeatTimeout(), getSelf());
+    //         }
+    //     }
+    // }
 
-    public final void startCoordinatorHeartbeat() {
-        // debug("Replica " + this.id + " starting coordinator heartbeat");
-        getContext().getSystem().scheduler().scheduleAtFixedRate(
-                Duration.create(getCoordinatorBeatInterval(), TimeUnit.MILLISECONDS),
-                Duration.create(getCoordinatorBeatInterval(), TimeUnit.MILLISECONDS),
-                getSelf(),
-                new Messages.Heartbeat(),
-                getContext().dispatcher(),
-                getSelf());
-    }
 
-    private void resetHeartbeatTimeout() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resetHeartbeatTimeout'");
-    }
 
     @Override
     public int getSystemNumberOfActors() {
@@ -263,7 +279,11 @@ public class Replica extends AbstractReplica {
 
     @Override
     public void crash(AbstractReplica.Crash how_to_crash) {
-        // TODO: implement
+        // change state: NORMAL/ELECTION -> CRASH    
+        // this is done by changing the node behaviour using the "message filter" defined in createCrashedReceive
+        // this way it stops handling messages
+        getContext().become(createCrashedReceive());
+
     }
 
     @Override
@@ -279,7 +299,7 @@ public class Replica extends AbstractReplica {
 
     }
 
-    // This methods handle message reception in different situations: NORMAL, ELECTION, CRASHED
+    // this methods handle message reception in different situations: NORMAL, ELECTION, CRASHED
     // .match() filters messages the replica can receive/handle in each state
 
     // NORMAL state: the replica is wroking normally, no crash detected
@@ -291,13 +311,13 @@ public class Replica extends AbstractReplica {
                 .match(Messages.Update.class, this::handleUpdate)
                 .match(Messages.Ack.class, this::handleAck)
                 .match(Messages.WriteOk.class, this::handleWriteOk)
+                .match(Messages.Heartbeat.class, this::handleHeartbeat) // handle heartbeat
 
                 // Also handle the timers
                 .match(Messages.HeartbeatTimeout.class, this::handleHeartbeatTimeout)
                 .match(Messages.UpdateTimeout.class, this::handleUpdateTimeout)
                 .match(Messages.WriteOkTimeout.class, this::handleWriteOkTimeout)
                 .match(Messages.ReadRequest.class, this::handleReadRequest)
-                .match(Messages.Heartbeat.class, this::handleHeartbeat)
                 .build();
     }
 
@@ -314,18 +334,84 @@ public class Replica extends AbstractReplica {
                 .build();
     }
 
-    // Method to handle timeouts
-    public final void handleHeartbeatTimeout(Messages.HeartbeatTimeout _msg) throws Exception {
-
-
-    }
-
+    // TIMEOUTS MANAGEMENT
+    // handle Update message timeout
     public final void handleUpdateTimeout(Messages.UpdateTimeout _msg) throws Exception {
-
+        // myTODO: cancel + election + message as in heartbeat
     }
 
+    // handle WriteOk message timeout
     public final void handleWriteOkTimeout(Messages.WriteOkTimeout _msg) throws Exception {
+        // myTODO: cancel + election + message as in heartbeat
+    }
 
+    // HEARTBEAT MANAGEMENT
+    // start heartbeat (x coordinator)
+    public final void startCoordinatorHeartbeat() {
+        // debug("Replica " + this.id + " starting coordinator heartbeat");
+        getContext().getSystem().scheduler().scheduleAtFixedRate(
+                Duration.create(getCoordinatorBeatInterval(), TimeUnit.MILLISECONDS),
+                Duration.create(getCoordinatorBeatInterval(), TimeUnit.MILLISECONDS),
+                getSelf(),
+                new Messages.Heartbeat(),
+                getContext().dispatcher(),
+                getSelf());
+    }
+
+    // reset heartbeat timer on heartbeat message reception (x node)
+    private void resetHeartbeatTimeout() {
+        // cancel old timer (if it exists)
+        if(heartbeatTimer != null){
+            heartbeatTimer.cancel();
+        }
+        
+        // start new one
+        heartbeatTimer = getContext().system().scheduler().scheduleOnce(
+            Duration.create(getCoordinatorBeatInterval() * 2, TimeUnit.MILLISECONDS), // timer duration here depends on coordinator heartbeat duration
+            getSelf(),                                                                // destination (self)
+            new Messages.HeartbeatTimeout(),                                          // message that will be received, here HeartbeatTimeout
+            getContext().dispatcher(),                                                // dispatcher
+            getSelf()                                                                 // sender (self)
+        );
+    }
+
+    // handle heartbeat message (x nodes and coordinator)
+    public final void handleHeartbeat(Messages.Heartbeat _msg) throws Exception {
+        // coordinator is in charge of telling the nodes that it is alive
+        if (this.id == coordinatorId){ 
+            for (Map.Entry<Integer, ActorRef> entry : group.entrySet()) {
+                if (entry.getKey() != this.id) { // get nodes that are not the cooridnator
+                    entry.getValue().tell(new Messages.Heartbeat(), getSelf()); // tell them coordinator is alive
+                }
+            }
+        }
+        else{ 
+            // when other nodes receive a heartbeat from the coordinator they can reset the timer
+            resetHeartbeatTimeout();
+        }
+    }
+
+    // handle heartbeat message timeout -> coordinator crashed!
+    public final void handleHeartbeatTimeout(Messages.HeartbeatTimeout _msg) throws Exception {
+        // log info
+        Logger.log("Heartbeat timeout detected by node " + this.id + ". Starting election protocol.");
+
+        // cancel all timers, not needed anymore
+        cancelAllTimers();
+        
+        // change state: NORMAL -> ELECTION    
+        // this is done by changing the node behaviour using the "message filter" defined in createElectionReceive
+        getContext().become(createElectionReceive());
+
+        // start election
+        // TODO: send election message to next node in the ring
+    }
+
+    // utility function to cancel all timers
+    public final void cancelAllTimers(){
+        updateTimer.cancel();
+        writeOkTimer.cancel();
+        heartbeatTimer.cancel();
     }
 
 }
